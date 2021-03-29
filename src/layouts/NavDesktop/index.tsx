131897bb89c0d4b86f8592e4e432e88006295dcf
Pavlo Getta <pavlo.getta@epfl.ch>
2021-03-29 07:29:25 +0200

    Make contact us link to external page

--- src/layouts/NavDesktop/index.tsx
+++ src/layouts/NavDesktop/index.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-// import { NavLink, withRouter } from 'react-router-dom';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-// import { useSelector } from 'react-redux';
 
 import Button from '../../components/Button';
 import { HomeNav, SecondaryNav } from '../Navigation';
-// import { State } from '../../store';
 
-// import './style.scss';
 
 const classPrefix = 'nav-desktop__';
 
 type NavButtonProps = {
   path: string;
+  external?: boolean;
   name: string;
   notifications?: number;
   home?: boolean;
@@ -23,27 +20,28 @@ type NavButtonProps = {
 
 const NavButton: React.FC<NavButtonProps> = ({
   path,
+  external = false,
   name,
   notifications,
   home,
   highlight,
   onClick,
 }) => {
-  return (
-    <Link href={path}>
-      <a>
-        <Button
-          width={highlight || home ? 140 : undefined}
-          discrete={!highlight && !home}
-          onClick={onClick}
-          notifications={notifications}
-          uppercase
-        >
-          {name}
-        </Button>
-      </a>
-    </Link>
+  const button = (
+    <Button
+      width={highlight || home ? 140 : undefined}
+      discrete={!highlight && !home}
+      onClick={onClick}
+      notifications={notifications}
+      uppercase
+    >
+      {name}
+    </Button>
   );
+
+  return external
+    ? (<a href={path} target="_blank">{button}</a>)
+    : (<Link href={path}><a>{button}</a></Link>);
 };
 
 const NavDesktop = () => {
@@ -51,9 +49,6 @@ const NavDesktop = () => {
 
   const [secondaryNav, setSecondaryNav] = React.useState(false);
   React.useEffect(() => setSecondaryNav(false), [router]);
-  // const downloadItems = useSelector<State, number>(
-  //   state => state.download.items.length,
-  // )
 
   return (
     <ul className={`${classPrefix}basis`}>
@@ -77,22 +72,14 @@ const NavDesktop = () => {
           </div>
         )}
       </li>
-      {/* <li>
-        <NavButton
-          path="/downloads"
-          name="Download"
-          // notifications={downloadItems > 0 ? downloadItems : undefined}
-          notifications={undefined}
-          highlight
-        />
-      </li> */}
       <li>
         <NavButton path="/glossary" name="Glossary" />
       </li>
       <li>
         <NavButton
-          path="/contact"
+          path="/#contact"
           name="Contact Us"
+          external
         />
       </li>
     </ul>
