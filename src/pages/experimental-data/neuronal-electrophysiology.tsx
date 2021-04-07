a6cd7602478d96971ee647707ad576f9670ab82c
Pavlo Getta <pavlo.getta@epfl.ch>
2021-04-07 22:10:25 +0200

    Do not defer systemjs scripts

--- src/pages/experimental-data/neuronal-electrophysiology.tsx
+++ src/pages/experimental-data/neuronal-electrophysiology.tsx
@@ -8,10 +8,11 @@ export default function About() {
   return (
     <MainLayout>
       <Head>
-        <script defer src="https://www.unpkg.com/systemjs@6.1.7/dist/system.js"></script>
-        <script defer src="https://www.unpkg.com/systemjs@6.1.7/dist/extras/named-exports.js"></script>
-        <script type="systemjs-importmap" src="/model/systemjs-importmap.json"></script>
+        <script key="sysjs" src="https://www.unpkg.com/systemjs@6.1.7/dist/system.js" />
+        <script key="sysjs-named-exports" src="https://www.unpkg.com/systemjs@6.1.7/dist/extras/named-exports.js" />
+        <script key="sysjs-importmap" type="systemjs-importmap" src="/model/systemjs-importmap.json" />
       </Head>
+
       <NeuronEphysView />
     </MainLayout>
   );
