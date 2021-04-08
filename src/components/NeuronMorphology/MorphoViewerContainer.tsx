d6d81d1f35a214a4e234a07a1140ca512d69563b
Pavlo Getta <pavlo.getta@epfl.ch>
2021-04-08 14:28:24 +0200

    Use polyline mode as default for staticly served morphologies

--- src/components/NeuronMorphology/MorphoViewerContainer.tsx
+++ src/components/NeuronMorphology/MorphoViewerContainer.tsx
@@ -18,7 +18,7 @@ const MorphoViewerContainer: React.FC<{
   });
 
   const [options, setOptions] = React.useState<MorphoViewerOptions>({
-    asPolyline: false,
+    asPolyline: true,
     focusOn: true,
     somaMode: 'fromOrphanSections',
   });
