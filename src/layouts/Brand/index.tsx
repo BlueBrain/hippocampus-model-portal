27b4ba3178ef009e1f0731380083ef74948c07fd
Pavlo Getta <pavlo.getta@epfl.ch>
2021-04-12 21:21:26 +0200

    Make home links point to Hub home, always open in the same windo

--- src/layouts/Brand/index.tsx
+++ src/layouts/Brand/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 
 import { basePath } from '../../config';
 
@@ -10,19 +9,17 @@ const classPrefix = 'brand__';
 const Brand: React.FC = () => {
   return (
     <div className={`${classPrefix}basis`}>
-      <Link href="/">
-        <a>
-          <img
-            className="logo"
-            src={`${basePath}/assets/images/epfl-logo.svg`}
-            alt="EPFL logo"
-          />
-          <div className="divider"></div>
-          <span className="text-grey">Blue Brain Project</span>
-          <div className="divider second-divider"></div>
-          <span className="hub-explore">The Hippocampus Hub Explore</span>
-        </a>
-      </Link>
+      <a href="/">
+        <img
+          className="logo"
+          src={`${basePath}/assets/images/epfl-logo.svg`}
+          alt="EPFL logo"
+        />
+        <div className="divider"></div>
+        <span className="text-grey">Blue Brain Project</span>
+        <div className="divider second-divider"></div>
+        <span className="hub">The Hippocampus Hub</span>
+      </a>
     </div>
   );
 };
--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 
 import styles from './styles.module.scss';
 
@@ -14,7 +13,6 @@ const Footer: React.FC = () => {
             <p>
               <a
                 href="https://www.epfl.ch/research/domains/bluebrain/"
-                target="_blank"
                 rel="noopener"
               >
                 Blue Brain Project
@@ -27,40 +25,22 @@ const Footer: React.FC = () => {
           </div>
           <div className="col-xs-12 col-sm-4">
             <p>
-              <a
-                href="/model/terms-of-use/"
-                target="_blank"
-              >
-                Terms of Use
-              </a>
-              - EPFL/BBP
+              <a href="/model/terms-of-use/">Terms of Use</a> - EPFL/BBP
             </p>
             <p>
-              <a
-                href="/model/privacy-policy/"
-                target="_blank"
-              >
-                Privacy Policy
-              </a>
-              - EPFL/BBP
+              <a href="/model/privacy-policy/">Privacy Policy</a> - EPFL/BBP
             </p>
             <p>
-              <a
-                href="/model/cookies-policy/"
-                target="_blank"
-              >
-                Cookies Policy
-              </a>
-              - EPFL/BBP
+              <a href="/model/cookies-policy/">Cookies Policy</a> - EPFL/BBP
             </p>
           </div>
           <div className="col-xs-12 col-sm-4">
-            <p><Link href="/">Home</Link></p>
+            <p><a href="/">Home</a></p>
             <p><a href="/build/">Build Models</a></p>
             <p><a href="/model/">Explore Models</a></p>
-            <p><a href="/#resources" target="_blank">Resources</a></p>
-            <p><a href="/#terms" target="_blank">Terms & Conditions</a></p>
-            <p><a href="/#contact-us" target="_blank">Contact Us</a></p>
+            <p><a href="/#resources">Resources</a></p>
+            <p><a href="/#terms">Terms & Conditions</a></p>
+            <p><a href="/#contact-us">Contact Us</a></p>
           </div>
         </div>
         <div className="mt-2">
