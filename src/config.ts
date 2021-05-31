d0aa8b562162934bd7860982bcfa5f91440c5241
Pavlo Getta <pavlo.getta@epfl.ch>
2021-05-31 13:07:31 +0200

    Add .env files

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,4 @@
 
-export const hippocampus = {
-  org: 'public',
-  project: 'hippocampus',
-  datasetViewId: encodeURIComponent(
-    'https://bbp.epfl.ch/neurosciencegraph/data/views/es/dataset',
-  ),
-};
-
-export const nexus = {
-  url: process.env.NEXT_PUBLIC_NEXUS_URL || 'https://bbp.epfl.ch/nexus/v1',
-  token: process.env.NEXT_PUBLIC_NEXUS_TOKEN,
-};
-
-export const sentryDsn = process.env.NEXT_PUBLIC_SENTRY_DSN;
-
-export const isProduction = process.env.NODE_ENV === 'production';
-
-export const nexusPluginBaseUrl = 'https://bbp.epfl.ch/nexus/plugins';
-
 export const accentColors: { [key: string]: string } = {
   yellow: '#ffc500',
   blue: '#84bbf8',
@@ -27,7 +8,28 @@ export const accentColors: { [key: string]: string } = {
   orange: '#ed8048',
 };
 
+export const hippocampus = {
+  org: 'public',
+  project: 'hippocampus-hub',
+  datasetViewId: encodeURIComponent('https://bbp.epfl.ch/neurosciencegraph/data/views/es/dataset'),
+};
+
+export const deploymentUrl = 'https://hippocampushub.eu';
 export const basePath = '/model';
+
+export const isServer = typeof window === 'undefined';
+export const isProduction = process.env.NODE_ENV === 'production';
+
+export const nexus = {
+  url: process.env.NEXT_PUBLIC_NEXUS_URL,
+  token: process.env.NEXT_PUBLIC_NEXUS_TOKEN,
+};
+
+export const sentryDsn = process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+export const nexusPluginBaseUrl = process.env.NEXT_PUBLIC_NEXUS_PLUGIN_BASE_URL;
+
+export const feedbackUrl = process.env.NEXT_PUBLIC_FEEDBACK_URL;
 
 export const gtm = {
   id: 'G-SGZ83Y6E8H',
