3ac5cff06df1bcc5afb59a5d0ef940a6173e11b6
Pavlo Getta <pavlo.getta@epfl.ch>
2022-01-03 15:59:18 +0100

    Enable bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 const { withSentryConfig } = require('@sentry/nextjs');
 
+const withBundleAnalyzer = require('@next/bundle-analyzer') ({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const SentryWebpackPluginOptions = {
   silent: true,
@@ -39,4 +42,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withSentryConfig(nextConfig, SentryWebpackPluginOptions);
+module.exports = withBundleAnalyzer(withSentryConfig(nextConfig, SentryWebpackPluginOptions));
