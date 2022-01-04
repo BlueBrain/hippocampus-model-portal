373db8212a8e3809685ed7488762ab02463058cc
Pavlo Getta <pavlo.getta@epfl.ch>
2022-01-04 10:39:39 +0100

    Remove noindex nofollow directive

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,7 +29,6 @@ function App({ Component, pageProps }) {
   return (
     <NexusProvider nexusClient={nexusClient}>
       <Head>
-        <meta name="robots" content="noindex,nofollow" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;700&display=swap" />
         <link rel="shortcut icon" href="https://www.hippocampushub.eu/favicon.ico" />
