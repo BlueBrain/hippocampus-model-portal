5d4e9bbe24f5cce76bc62c377668c7b460d44a3b
Pavlo Getta <pavlo.getta@epfl.ch>
2022-01-18 07:35:13 +0100

    Make NexusFileDownloadButton component load dynamically

--- src/components/NexusFileDownloadButton/index.tsx
+++ src/components/NexusFileDownloadButton/index.tsx
@@ -1,58 +1,16 @@
-import React, { useState } from 'react';
-import { useNexusContext } from '@bbp/react-nexus';
-import { saveAs } from 'file-saver';
-import { Button } from 'antd';
+import React from 'react';
+import dynamic from 'next/dynamic';
 
-import { parseUrl } from '../../utils';
+import { NexusFileDownloadButtonProps } from './NexusFileDownloadButton';
 
 
+const NexusFileDownloadButtonLazy = dynamic(() => import('./NexusFileDownloadButton'), { ssr: false });
 
-export type NexusFileDownloadButtonProps = {
-  filename: string;
-  url: string;
-  org?: string;
-  project?: string;
-  children: React.ReactNode;
-  className?: string;
-  id?: string;
-};
-
-const NexusFileDownloadButton: React.FC<NexusFileDownloadButtonProps> = ({
-  filename,
-  url,
-  org,
-  project,
-  children,
-  className = '',
-  id,
-}) => {
-  const nexus = useNexusContext();
-
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const download = () => {
-    setLoading(true);
-    const { org: fileOrg, project: fileProject } = {...parseUrl(url), ...{ org, project }};
-    const fileId = url.split('/').reverse()[0];
-
-    nexus.File.get(fileOrg as string, fileProject as string, fileId, { as: 'blob' })
-      .then(imageData => saveAs(imageData as Blob, filename))
-      .catch(console.error)
-      .finally(() => setLoading(false));
-  };
-
+const NexusFileDownloadButton: React.FC<NexusFileDownloadButtonProps> = (props) => {
   return (
-    <Button
-      id={id}
-      className={className}
-      type="primary"
-      size="small"
-      onClick={download}
-      loading={loading}
-    >
-      {children}
-    </Button>
+    <NexusFileDownloadButtonLazy {...props} />
   );
 };
+
 
 export default NexusFileDownloadButton;
