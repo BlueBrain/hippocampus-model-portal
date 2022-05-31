c9990b8ad244e2aefa6ca85145cde1353f841dd6
Pavlo Getta <pavlo.getta@epfl.ch>
2022-05-31 16:15:25 +0200

    Remove old simplification code

--- src/components/ConnectionViewer/workers/neuron-geometry.ts
+++ src/components/ConnectionViewer/workers/neuron-geometry.ts
@@ -36,31 +36,29 @@ function simplify(pts) {
   }
 }
 
-function _createSecGeometryFromPoints(pts, simplificationRatio = 1) {
-  const sRatio = simplificationRatio;
-
+function _createSecGeometryFromPoints(pts) {
   simplify(pts);
 
   const geometries = [];
 
-  for (let i = 0; i < pts.length - 1; i += sRatio) {
+  for (let i = 0; i < pts.length - 1; i += 1) {
     const vstart = new Vector3(pts[i][0], pts[i][1], pts[i][2]);
     const vend = new Vector3(
-      pts[i + sRatio] ? pts[i + sRatio][0] : last(pts)[0],
-      pts[i + sRatio] ? pts[i + sRatio][1] : last(pts)[1],
-      pts[i + sRatio] ? pts[i + sRatio][2] : last(pts)[2],
+      pts[i + 1][0],
+      pts[i + 1][1],
+      pts[i + 1][2],
     );
     const distance = vstart.distanceTo(vend);
     const position = vend.clone().add(vstart).divideScalar(2);
 
-    const dStart = pts[i][3] * 2;
-    const dEnd = (pts[i + sRatio] ? pts[i + sRatio][3] : last(pts)[3]) * 2;
+    const dStart = pts[i][3] / 2;
+    const dEnd = pts[i + 1][3] / 2;
 
     const geometry = new CylinderGeometry(
       dStart,
       dEnd,
       distance,
-      Math.max(5, Math.ceil(24 / sRatio)),
+      12,
       1,
       true,
     );
