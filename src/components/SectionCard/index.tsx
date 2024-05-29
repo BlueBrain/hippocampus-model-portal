3f2898e45ae9cdf1d701856418df9c51c7f6b90d
Benjamin Botros <benjaminbotros@outlook.com>
2024-05-29 17:19:18 +0200

    Add Placeholder BG

--- src/components/SectionCard/index.tsx
+++ src/components/SectionCard/index.tsx
@@ -1,14 +1,18 @@
 import React, { ReactNode, useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { FaMinus, FaPlus } from 'react-icons/fa';
 
 import styles from './styles.module.scss';
+
+import { Row, Col } from 'antd';
 
 
 type SectionCardProps = {
   title: string;
   description?: ReactNode;
   idx: string;
+  icon?: string;
   bgColor?: string;
   links: {
     label: string;
@@ -21,6 +25,7 @@ const SectionCard: React.FC<SectionCardProps> = ({
   description,
   idx,
   links,
+  icon,
 }) => {
   const [infoOpened, setInfoOpened] = useState(false);
 
@@ -28,7 +33,11 @@ const SectionCard: React.FC<SectionCardProps> = ({
     <div className={`${styles.container} bg-grey-${idx} ${infoOpened ? 'show' : ''}`}>
       <div className={styles.head}>
         <div className={styles.title}>
-          <div className={styles.idx}>0{idx}</div>
+          <Row justify="space-between" align="middle">
+            <Col>{icon && <img src={icon} alt="icon" className={styles.icon} />}</Col>
+            <Col><div className={styles.idx}>0{idx}</div></Col>
+          </Row>
+
           <h3 className="text-white">{title}</h3>
         </div>
       </div>
@@ -45,7 +54,7 @@ const SectionCard: React.FC<SectionCardProps> = ({
           className={styles.infoBtn}
           onClick={() => setInfoOpened(!infoOpened)}
         >
-          {infoOpened ? (<FaMinus size={14}/>) : (<FaPlus size={14}/>)}
+          {infoOpened ? (<FaMinus size={14} />) : (<FaPlus size={14} />)}
         </div>
 
         <div
