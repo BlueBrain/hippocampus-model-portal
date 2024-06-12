40936b54b014d926ab66901f7abc525f4e079054
Benjamin Botros <benjaminbotros@outlook.com>
2024-06-12 09:16:32 +0200

    Adding text content

--- src/views/reconstruction-data/CellComposition.tsx
+++ src/views/reconstruction-data/CellComposition.tsx
@@ -55,15 +55,8 @@ const CellCompositionView: React.FC = () => {
               />
               <div role="information">
                 <InfoBox>
-                  <p className="text-tmp">
-                    Vivamus vel semper nisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-                    per inceptos himenaeos. Vivamus ipsum enim, fermentum quis ipsum nec, euismod convallis leo. <br />
-                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                    Sed vel scelerisque felis, quis condimentum felis. Pellentesque dictum neque vel mauris dignissim,
-                    vitae ornare arcu sagittis. <br />
-                    Etiam vestibulum, nisi in scelerisque porta, enim est gravida mi,
-                    nec pulvinar enim ligula non lorem. Aliquam ut orci est.
-                    Praesent tempus sollicitudin ante varius feugiat.
+                  <p>
+                    We combined information on available morphological reconstructions [hyperlink], electrophysiological recordings [hyperlink], cell density [hyperlink], volume [hyperlink], and the estimation from Bezaire and Soltesz (2013) [hyperlink] to predict the number of neuron types.
                   </p>
                 </InfoBox>
               </div>
@@ -99,11 +92,8 @@ const CellCompositionView: React.FC = () => {
           id="cellCompositionSection"
           title="Cell composition"
         >
-          <p className="text-tmp mb-3">
-            Et quibusdam sunt et accusamus nihil aut officia alias vel galisum laudantium et consequatur adipisci ut
-            sint quaerat? Aut mollitia excepturi id adipisci internos et aliquam repellat aut aperiam odit rem earum
-            facere vel sequi consequatur ut soluta obcaecati. Non galisum accusantium ut iusto eius aut doloribus
-            omnis eum quasi sint nam omnis aspernatur.
+          <p className="mb-3">
+            Density and number of cells for each morphological type (m-type).
           </p>
 
           <CellCompositionTable volumeSection={volumeSection} />
@@ -114,11 +104,8 @@ const CellCompositionView: React.FC = () => {
           id="MECompositionSection"
           title="ME-composition"
         >
-          <p className="text-tmp mb-3">
-            Et quibusdam sunt et accusamus nihil aut officia alias vel galisum laudantium et consequatur adipisci ut
-            sint quaerat? Aut mollitia excepturi id adipisci internos et aliquam repellat aut aperiam odit rem earum
-            facere vel sequi consequatur ut soluta obcaecati. Non galisum accusantium ut iusto eius aut doloribus
-            omnis eum quasi sint nam omnis aspernatur.
+          <p className="mb-3">
+            Each morphological type (m-type) can show one or more electrical types (e-types) giving rise to different morpho-electrical types (me-types).
           </p>
 
           <MECompositionTable />
