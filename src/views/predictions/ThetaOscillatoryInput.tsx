5243beb0ba0f460cc02568c518745f7be11aa929
Benjamin Botros <benjaminbotros@outlook.com>
2024-06-14 10:23:58 +0200

    Adding Views

---
+++ src/views/predictions/ThetaOscillatoryInput.tsx
@@ -0,0 +1,67 @@
+
+import React from 'react';
+import { Row, Col } from 'antd';
+import Image from 'next/image';
+
+
+import Filters from '@/layouts/Filters';
+import StickyContainer from '@/components/StickyContainer';
+
+import Title from '@/components/Title';
+import InfoBox from '@/components/InfoBox';
+import selectorStyle from '@/styles/selector.module.scss';
+
+
+const ThetaOscillatoryInputView: React.FC = () => {
+
+    const theme = 5;
+
+    return (
+        <>
+            <Filters>
+                <Row
+                    className="w-100"
+                    gutter={[0, 20]}
+                >
+                    <Col
+                        className="mb-2"
+                        xs={24}
+                        lg={12}
+                    >
+                        <StickyContainer>
+                            <Title
+                                title="Theta - Oscillatory input"
+                                subtitle="Predictions"
+                                theme={theme}
+                            />
+                            <div role="information">
+                                <InfoBox>
+                                    <p>
+
+                                    </p>
+                                </InfoBox>
+                            </div>
+                        </StickyContainer>
+                    </Col>
+                    <Col
+                        className={`set-accent-color--${'grey'} mb-2`}
+                        xs={24}
+                        lg={12}
+                    >
+                        <div className={selectorStyle.selector} style={{ maxWidth: '26rem' }}>
+                            <div className={selectorStyle.selectorColumn}>
+                                <div className={selectorStyle.selectorBody}>
+
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Filters>
+
+        </>
+    );
+};
+
+
+export default ThetaOscillatoryInputView;
