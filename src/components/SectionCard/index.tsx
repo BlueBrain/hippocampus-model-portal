a451007714ef9447ad56532fbc396b2bc65f471a
Benjamin Botros <benjaminbotros@outlook.com>
2024-07-01 15:41:03 +0200

    creating pop up logic

--- src/components/SectionCard/index.tsx
+++ src/components/SectionCard/index.tsx
@@ -1,7 +1,8 @@
 import React, { ReactNode, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { FaMinus, FaPlus } from 'react-icons/fa';
+import { FaMinus, FaPlus, FaInfoCircle } from 'react-icons/fa';
+
 
 import styles from './styles.module.scss';
 
@@ -31,45 +32,49 @@ const SectionCard: React.FC<SectionCardProps> = ({
   const [infoOpened, setInfoOpened] = useState(false);
 
   return (
-    <div className={`${styles.container} bg-card-gradiant-${idx} ${infoOpened ? 'show' : ''}`}>
-      <div className={styles.head}>
-        <div className={styles.title}>
-          {/*
-
-          Icon & Count
-
+    <>
+      <div className={`${styles.container} bg-card-gradiant-${idx} ${infoOpened ? 'show' : ''}`}>
+        <div className={styles.head}>
           <Row justify="space-between" align="middle">
-            <Col>{icon && <img src={icon} alt="icon" className={styles.icon} />}</Col>
-            <Col><div className={styles.idx}>0{idx}</div></Col>
+            <Col>
+              <div className={styles.title}>
+                <h3 className="text-white">{title}</h3>
+              </div>
+            </Col>
+            <Col><FaInfoCircle className={styles.icon} onClick={() => setInfoOpened(!infoOpened)} /></Col>
           </Row>
-          */}
-          <h3 className="text-white">{title}</h3>
+        </div>
+        <div className={styles.body}>
+          {links.map(link => link.href ? (
+            <Link key={link.label} href={link.href} prefetch={false} legacyBehavior>
+              {link.label}
+            </Link>
+          ) : (
+            <p key={link.label}>{link.label}<sup>*</sup></p>
+          ))}
+          {/*
+          <div
+            className={styles.infoBtn}
+            onClick={() => setInfoOpened(!infoOpened)}
+          >
+            {infoOpened ? (<FaMinus size={14} />) : (<FaPlus size={14} />)}
+          </div>
+
+          <div
+            className={styles.info + ' bg-card-gradiant-' + idx}
+          >
+            {description}
+          </div>
+          <div className={styles.infoBottomGradient}></div>
+           */}
         </div>
       </div>
-      <div className={styles.body}>
-        {links.map(link => link.href ? (
-          <Link key={link.label} href={link.href} prefetch={false} legacyBehavior>
-            {link.label}
-          </Link>
-        ) : (
-          <p key={link.label}>{link.label}<sup>*</sup></p>
-        ))}
+      <div className={`${styles.popup} ${infoOpened ? styles.show : ''}`}>
 
-        <div
-          className={styles.infoBtn}
-          onClick={() => setInfoOpened(!infoOpened)}
-        >
-          {infoOpened ? (<FaMinus size={14} />) : (<FaPlus size={14} />)}
-        </div>
-
-        <div
-          className={styles.info + ' bg-card-gradiant-' + idx}
-        >
-          {description}
-        </div>
-        <div className={styles.infoBottomGradient}></div>
       </div>
-    </div>
+
+
+    </>
   );
 };
 
