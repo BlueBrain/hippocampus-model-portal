a14c52de95cadbe31bfa7b3423c0585cc9accb5f
Benjamin Botros <benjaminbotros@outlook.com>
2024-08-08 09:23:48 +0200

    Connection Anatomy Page

--- src/views/validations/ConnnectionAnatomy.tsx
+++ src/views/validations/ConnnectionAnatomy.tsx
@@ -6,7 +6,17 @@ import StickyContainer from '@/components/StickyContainer';
 
 import Title from '@/components/Title';
 import InfoBox from '@/components/InfoBox';
+import DataContainer from '@/components/DataContainer';
+import Collapsible from '@/components/Collapsible';
 
+import BoutonDensityValidation from './connection-anatomy/BoutonDensityValidation';
+import ConnectionProbability from './connection-anatomy/ConnectionProbability';
+import ConvergenceValidation from './connection-anatomy/ConvergenceValidation';
+import DivergenceValidation from './connection-anatomy/DivergenceValidation';
+import ExperimentalLaminarDistributionOfSynapses from './connection-anatomy/ExperimentalLaminarDistributionOfSynapses';
+import ModelLaminarDistributionOfDynapses from './connection-anatomy/ModelLaminarDistributionOfDynapses';
+import NbOfSynapsesPConnection from './connection-anatomy/NbOfSynapsesPConnection';
+import SynapticDivergencePercentages from './connection-anatomy/SynapticDivergencePercentages';
 
 const ConnectionAnatomyView: React.FC = () => {
 
@@ -34,8 +44,52 @@ const ConnectionAnatomyView: React.FC = () => {
                     </div>
                 </div>
             </Filters>
+            <DataContainer theme={theme}
+                navItems={[
+                    { id: 'BoutonDensityValidationSection', label: 'Bouton density validation' },
+                    { id: 'ConnectionProbabilitySection', label: 'Connection probabilites validation' },
+                    { id: 'ConvergenceValidationSection', label: 'Convergence validation' },
+                    { id: 'DivergenceValidationSection', label: 'Divergence validation' },
+                    { id: 'ExperimentalLaminarDistributionOfSynapsesSection', label: 'Experimental Laminar distribution of synapses' },
+                    { id: 'ModelLaminarDistributionOfDynapsesSection', label: 'Model Laminar distribution of synapses' },
+                    { id: 'NbOfSynapsesPConnectionSection', label: 'Number of Synapse per connection validation' },
+                    { id: 'SynapticDivergencePercentagesSection', label: '' },
+                ]}>
+
+                <Collapsible id="BoutonDensityValidationSection" title={`Bouton density validation`}>
+                    <BoutonDensityValidation theme={theme} />
+                </Collapsible>
+
+                <Collapsible id="ConnectionProbabilitySection" title={`Connection probabilites validation`}>
+                    <ConnectionProbability theme={theme} />
+                </Collapsible>
+
+                <Collapsible id="ConvergenceValidationSection" title={`Convergence validation`}>
+                    <ConvergenceValidation theme={theme} />
+                </Collapsible>
+
+                <Collapsible id="DivergenceValidationSection" title={`Divergence validation`}>
+                    <DivergenceValidation theme={theme} />
+                </Collapsible>
+
+                <Collapsible id="ExperimentalLaminarDistributionOfSynapsesSection" title={`Experimental Laminar distribution of synapses`}>
+                    <ExperimentalLaminarDistributionOfSynapses theme={theme} />
+                </Collapsible>
+
+                <Collapsible id="ModelLaminarDistributionOfDynapsesSection" title={`Model Laminar distribution of synapses`}>
+                    <ModelLaminarDistributionOfDynapses theme={theme} />
+                </Collapsible>
+
+                <Collapsible id="NbOfSynapsesPConnectionSection" title={`Number of Synapse per connection validation`}>
+                    <NbOfSynapsesPConnection theme={theme} />
+                </Collapsible>
+
+                <Collapsible id="SynapticDivergencePercentagesSection" title={`Synaptic divergence percetages`}>
+                    <SynapticDivergencePercentages theme={theme} />
+                </Collapsible>
 
 
+            </DataContainer >
         </>
     );
 };
