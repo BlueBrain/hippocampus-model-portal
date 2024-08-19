4c12cefb1019fe24b3ff288408a06351d72d6463
Benjamin Botros <benjaminbotros@outlook.com>
2024-08-19 09:52:17 +0200

    Media folder + remove unnecessary files

--- src/layouts/Brand/index.tsx
+++ src/layouts/Brand/index.tsx
@@ -12,7 +12,7 @@ const Brand: React.FC = () => {
       <a href="/">
         <img
           className="logo"
-          src={`${basePath}/data/epfl-logo-red.svg`}
+          src={`${basePath}/data/media/logos/epfl-logo-red.svg`}
           alt="EPFL logo"
         />
         <div className="divider"></div>
