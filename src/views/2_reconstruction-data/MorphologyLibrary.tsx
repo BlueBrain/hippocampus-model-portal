2bb3f2f28fd1be2fdd341a9d0633348a126e062e
Benjamin Botros <benjaminbotros@outlook.com>
2024-08-22 12:49:03 +0200

    Hero (with 3d not ok + adding mtypes)

--- src/views/2_reconstruction-data/MorphologyLibrary.tsx
+++ src/views/2_reconstruction-data/MorphologyLibrary.tsx
@@ -1,194 +1,214 @@
-import React from 'react';
-import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-
-import Title from '@/components/Title';
-import LayerSelector3D from '@/components/LayerSelector3D/index';
-import InfoBox from '@/components/InfoBox';
+import Link from 'next/link';
 import Filters from '@/layouts/Filters';
-import DataContainer from '@/components/DataContainer';
-import { Layer, QuickSelectorEntry } from '@/types';
+import StickyContainer from '@/components/StickyContainer';
+import Title from '@/components/Title';
+import InfoBox from '@/components/InfoBox';
 import List from '@/components/List';
+import LayerSelector3D from '@/components/LayerSelector3D';
+import DataContainer from '@/components/DataContainer';
 import Collapsible from '@/components/Collapsible';
-
-import models from '@/models.json';
+import HttpData from '@/components/HttpData';
+import { basePath } from '@/config';
 import { defaultSelection, layers } from '@/constants';
+import models from '@/models.json';
 import withPreselection from '@/hoc/with-preselection';
 import { colorName } from './config';
 
-const getMtypes = (layer: Layer): string[] => {
-  return layer
-    ? models
-      .filter(model => model.layer === layer)
-      .map(model => model.mtype)
-      .reduce((acc: string[], cur) => acc.includes(cur) ? acc : [...acc, cur], [])
-      .sort()
-    : [];
-}
-
-const getEtypes = (mtype: string): string[] => {
-  return mtype
-    ? models
-      .filter(model => model.mtype === mtype)
-      .map(model => model.etype)
-      .reduce((acc: string[], cur) => acc.includes(cur) ? acc : [...acc, cur], [])
-      .sort()
-    : [];
-}
-
-const getInstances = (mtype: string, etype: string): string[] => {
-  return etype
-    ? models
-      .filter(model => model.mtype === mtype && model.etype === etype)
-      .map(model => model.name)
-      .sort()
-    : [];
-}
-
-const Neurons: React.FC = () => {
+const MorphologyLibrary: React.FC = () => {
   const router = useRouter();
-  const theme = 3;
-
   const { query } = router;
-  const currentLayer: Layer = query.layer as Layer;
-  const currentMtype: string = query.mtype as string;
-  const currentEtype: string = query.etype as string;
-  const currentInstance: string = query.instance as string;
+
+  const [quickSelection, setQuickSelection] = useState<Record<string, string>>({
+    layer: '',
+    mtype: '',
+    etype: '',
+    instance: '',
+  });
+
+  const theme = 2;
+
+  useEffect(() => {
+    if (!router.isReady) return;
+
+    if (!query.layer && !query.mtype && !query.etype && !query.instance) {
+      const defaultQuery = defaultSelection.digitalReconstruction.neurons;
+      setQuickSelection(defaultQuery);
+      router.replace({ query: defaultQuery }, undefined, { shallow: true });
+    } else {
+      setQuickSelection({
+        layer: query.layer as string,
+        mtype: query.mtype as string,
+        etype: query.etype as string,
+        instance: query.instance as string,
+      });
+    }
+  }, [router.isReady, query]);
 
   const setParams = (params: Record<string, string>): void => {
-    const newQuery = {
-      ...{
-        layer: currentLayer,
-        mtype: currentMtype,
-        etype: currentEtype,
-        instance: currentInstance,
-      },
-      ...params,
-    };
-    router.push({ query: newQuery, pathname: router.pathname }, undefined, { shallow: true });
+    const newQuery = { ...router.query, ...params };
+    router.push({ query: newQuery }, undefined, { shallow: true });
   };
 
-  const setLayer = (layer: Layer) => {
-    setParams({
-      layer,
-      mtype: '',
-      etype: '',
-      instance: '',
-    })
-  };
-  const setMtype = (mtype: string) => {
-    setParams({
-      mtype,
-      etype: '',
-      instance: '',
-    })
-  };
-  const setEtype = (etype: string) => {
-    setParams({
-      etype,
-      instance: '',
-    })
-  };
-  const setInstance = (instance: string) => {
-    setParams({ instance })
+  const getMtypes = (layer: string) => {
+    return layer
+      ? Array.from(new Set(models.filter(m => m.layer === layer).map(m => m.mtype))).sort()
+      : [];
   };
 
-  const mtypes = getMtypes(currentLayer);
-  const etypes = getEtypes(currentMtype);
-  const instances = getInstances(currentMtype, currentEtype);
+  const getEtypes = (mtype: string) => {
+    return mtype
+      ? Array.from(new Set(models.filter(m => m.mtype === mtype).map(m => m.etype))).sort()
+      : [];
+  };
 
-  const qsEntries: QuickSelectorEntry[] = [
+  const getInstances = (mtype: string, etype: string) => {
+    return etype
+      ? models.filter(m => m.mtype === mtype && m.etype === etype).map(m => m.name).sort()
+      : [];
+  };
+
+  const setLayerQuery = (layer: string) => {
+    setQuickSelection(prev => {
+      const newMtypes = getMtypes(layer);
+      const newMtype = newMtypes.length > 0 ? newMtypes[0] : '';
+      const newEtypes = getEtypes(newMtype);
+      const newEtype = newEtypes.length > 0 ? newEtypes[0] : '';
+      const newInstances = getInstances(newMtype, newEtype);
+      const newInstance = newInstances.length > 0 ? newInstances[0] : '';
+
+      const updatedSelection = { layer, mtype: newMtype, etype: newEtype, instance: newInstance };
+      setParams(updatedSelection);
+      return updatedSelection;
+    });
+  };
+
+  const setMtypeQuery = (mtype: string) => {
+    setQuickSelection(prev => {
+      const newEtypes = getEtypes(mtype);
+      const newEtype = newEtypes.length > 0 ? newEtypes[0] : '';
+      const newInstances = getInstances(mtype, newEtype);
+      const newInstance = newInstances.length > 0 ? newInstances[0] : '';
+
+      const updatedSelection = { ...prev, mtype, etype: newEtype, instance: newInstance };
+      setParams(updatedSelection);
+      return updatedSelection;
+    });
+  };
+
+  const setEtypeQuery = (etype: string) => {
+    setQuickSelection(prev => {
+      const newInstances = getInstances(prev.mtype, etype);
+      const newInstance = newInstances.length > 0 ? newInstances[0] : '';
+
+      const updatedSelection = { ...prev, etype, instance: newInstance };
+      setParams(updatedSelection);
+      return updatedSelection;
+    });
+  };
+
+  const setInstanceQuery = (instance: string) => {
+    setQuickSelection(prev => {
+      const updatedSelection = { ...prev, instance };
+      setParams(updatedSelection);
+      return updatedSelection;
+    });
+  };
+
+  const mtypes = getMtypes(quickSelection.layer);
+  const etypes = getEtypes(quickSelection.mtype);
+  const instances = getInstances(quickSelection.mtype, quickSelection.etype);
+
+  const qsEntries = [
     {
       title: 'Layer',
       key: 'layer',
       values: layers,
-      setFn: setLayer,
+      setFn: setLayerQuery,
     },
     {
       title: 'M-type',
       key: 'mtype',
-      getValuesFn: getMtypes,
-      getValuesParam: 'layer',
-      setFn: setMtype,
+      values: mtypes,
+      setFn: setMtypeQuery,
     },
     {
-      title: 'E-Type',
+      title: 'E-type',
       key: 'etype',
-      getValuesFn: getEtypes,
-      getValuesParam: 'mtype',
-      setFn: setEtype,
+      values: etypes,
+      setFn: setEtypeQuery,
     },
     {
       title: 'Instance',
       key: 'instance',
-      getValuesFn: getInstances,
-      getValuesParam: ['mtype', 'etype'],
-      setFn: setInstance,
+      values: instances,
+      setFn: setInstanceQuery,
     },
   ];
 
   return (
     <>
-      <Filters theme={theme}>
-        <div className="row bottom-xs w-100">
-          <div className="col-xs-12 col-lg-6">
-            <Title
-              primaryColor={colorName}
-              title="Morphology library"
-              subtitle="Reconstruction Data"
-              theme={theme}
-            />
-            <InfoBox color={colorName}>
-              <p>
-                We scale and clone <Link className={"link theme-" + theme} href={"/experimental-data/neuronal-morphology/"}>morphologies</Link> to produce a morphology library.
-              </p>
-            </InfoBox>
-          </div>
-
-          <div className="col-xs-12 col-lg-6">
-            <div className="selector">
-              <div className={"selector__column theme-" + theme}>
-                <div className={"selector__head theme-" + theme}>Choose a layer</div>
-                <div className={"selector__selector-container"}>
-                  <LayerSelector3D
-                    value={currentLayer}
-                    onSelect={setLayer}
-                    theme={theme}
-                  />
-                </div>
+      <Filters theme={theme} >
+        <div className="flex flex-col lg:flex-row w-full lg:items-center mt-40 lg:mt-0">
+          <div className="w-full lg:w-1/3 md:w-full md:flex-none mb-8 md:mb-8 lg:pr-0">
+            <StickyContainer>
+              <Title
+                title="Morphology library"
+                subtitle="Reconstruction Data"
+                theme={theme}
+              />
+              <div className='w-full' role="information">
+                <InfoBox>
+                  <p>
+                    We scale and clone <Link className={`link theme-${theme}`} href="/experimental-data/neuronal-morphology/">morphologies</Link> to produce a morphology library.
+                  </p>
+                </InfoBox>
               </div>
-              <div className={"selector__column theme-" + theme}>
-                <div className={"selector__head theme-" + theme}>Select reconstruction</div>
-                <div className={"selector__body"}>
-                  <List
-                    block
-                    list={mtypes}
-                    value={currentMtype}
-                    title={`M-type ${mtypes.length ? '(' + mtypes.length + ')' : ''}`}
-                    color={colorName}
-                    onSelect={setMtype}
-                    theme={theme}
-                  />
-                  <List
-                    block
-                    list={etypes}
-                    value={currentEtype}
-                    title={`E-type ${etypes.length ? '(' + etypes.length + ')' : ''}`}
-                    color={colorName}
-                    onSelect={setEtype}
-                    theme={theme}
-                  />
-                  <List
-                    block
-                    list={instances}
-                    value={currentInstance}
-                    title={`ME-type instance ${instances.length ? '(' + instances.length + ')' : ''}`}
-                    color={colorName}
-                    onSelect={setInstance}
-                    anchor="data"
-                    theme={theme}
-                  />
-                </div>
+            </StickyContainer>
+          </div>
+          <div className="flex flex-col-reverse md:flex-row-reverse gap-8 mb-12 md:mb-0 mx-8 md:mx-0 lg:w-2/3 md:w-full flex-grow md:flex-none">
+            <div className={`selector__column theme-${theme} w-full`}>
+              <div className={`selector__head theme-${theme}`}>Select reconstruction</div>
+              <div className="selector__body">
+                <List
+                  block
+                  list={mtypes}
+                  value={quickSelection.mtype}
+                  title={`M-type ${mtypes.length ? `(${mtypes.length})` : ''}`}
+                  color={colorName}
+                  onSelect={setMtypeQuery}
+                  theme={theme}
+                />
+                <List
+                  block
+                  list={etypes}
+                  value={quickSelection.etype}
+                  title={`E-type ${etypes.length ? `(${etypes.length})` : ''}`}
+                  color={colorName}
+                  onSelect={setEtypeQuery}
+                  theme={theme}
+                />
+                <List
+                  block
+                  list={instances}
+                  value={quickSelection.instance}
+                  title={`ME-type instance ${instances.length ? `(${instances.length})` : ''}`}
+                  color={colorName}
+                  onSelect={setInstanceQuery}
+                  anchor="data"
+                  theme={theme}
+                />
+              </div>
+            </div>
+            <div className={`selector__column theme-${theme} w-full`}>
+              <div className={`selector__head theme-${theme}`}>Choose a layer</div>
+              <div className="selector__body flex align-middle w-full">
+                <LayerSelector3D
+                  value={quickSelection.layer}
+                  onSelect={setLayerQuery}
+                  theme={theme}
+                />
               </div>
             </div>
           </div>
@@ -197,18 +217,25 @@ const Neurons: React.FC = () => {
 
       <DataContainer
         theme={theme}
+        visible={!!quickSelection.instance}
         navItems={[
           { id: 'morphologySection', label: 'Neuron Morphology' },
           { id: 'populationSection', label: 'Population' },
         ]}
         quickSelectorEntries={qsEntries}
       >
-        <Collapsible id="morphologySection" title={`Neuron Morphology ${currentMtype} ${currentEtype} ${currentInstance}`}>
-          <p>We provide visualization and morphometrics for the selected morphology.</p>
+        <Collapsible
+          id="morphologySection"
+          title="Neuron Morphology"
+          properties={[quickSelection.layer, quickSelection.mtype, quickSelection.etype, quickSelection.instance]}
+        >
+          <p></p>
         </Collapsible>
-
-        <Collapsible id="populationSection" title={`Population`}>
-          <p>We provide morphometrics for the entire m-type group selected.</p>
+        <Collapsible
+          id="populationSection"
+          title="Population"
+        >
+          <p></p>
         </Collapsible>
       </DataContainer>
     </>
@@ -216,7 +243,7 @@ const Neurons: React.FC = () => {
 };
 
 export default withPreselection(
-  Neurons,
+  MorphologyLibrary,
   {
     key: 'layer',
     defaultQuery: defaultSelection.digitalReconstruction.neurons,
