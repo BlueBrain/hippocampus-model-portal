72948f755743f2bb1d2b0edc5c7a517d4a5049c3
Benjamin Botros <benjaminbotros@outlook.com>
2024-09-27 16:18:04 +0200

    Validations

--- src/pages/validations/schaffer-collaterals-1.tsx
+++ src/pages/validations/schaffer-collaterals-1.tsx
@@ -1,22 +1,37 @@
-import React from 'react';
 import Head from 'next/head';
-
 import SchafferCollateralsView from '@/views/4_validations/SchafferCollaterals_1';
 
-
 export default function SchafferCollateralsPage() {
+  const metadata = {
+    title: 'Schaffer Collaterals 1 - Validation | The Hippocampus Hub',
+    description:
+      'We validated Schaffer collaterals at the synaptic level by comparing model predictions to experimental data on synaptic strength and spike time dynamics.',
+    keywords: [
+      'Schaffer Collaterals',
+      'Synaptic Strength',
+      'Spike Time Dynamics',
+      'Validation Data',
+      'Hippocampus',
+      'Neuroscience',
+      'Blue Brain Project',
+    ],
+    author: 'Blue Brain Project',
+    creator: 'EPFL Blue Brain Project',
+    publisher: 'The Hippocampus Hub',
+  };
+
   return (
     <>
       <Head>
-        <title>Schaffer collaterals 1 / Validations / Hippocampus Hub Explore</title>
-        {/* TODO: add description */}
-        <meta
-          name="description"
-          content=""
-        />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords.join(', ')} />
+        <meta name="author" content={metadata.author} />
+        <meta name="creator" content={metadata.creator} />
+        <meta name="publisher" content={metadata.publisher} />
       </Head>
 
       <SchafferCollateralsView />
     </>
   );
-};
+}
